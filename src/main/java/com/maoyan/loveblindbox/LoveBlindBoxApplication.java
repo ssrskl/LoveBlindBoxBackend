8ac8dd9da4757a01b4826992a71469b4060ab009
ssrskl <1071352028@qq.com>
2023-09-23 08:09:56 +0800

    初步搭建开发环境

---
+++ src/main/java/com/maoyan/loveblindbox/LoveBlindBoxApplication.java
@@ -0,0 +1,13 @@
+package com.maoyan.loveblindbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoveBlindBoxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoveBlindBoxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/maoyan/loveblindbox/LoveBlindBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maoyan.loveblindbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoveBlindBoxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
